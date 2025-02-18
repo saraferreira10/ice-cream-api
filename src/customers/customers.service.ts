5f984a19585cd9fba22c8917ec1257ce2e8d157e
Sara Ferreira <sara.f.araujo12@gmail.com>
2025-02-18 16:41:28 -0300

    test: created controllers unit tests

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { Repository } from 'typeorm';
+import { Customer } from './entities/customer.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CustomersService {
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customerRepository: Repository<Customer>,
+  ) {}
+
   create(createCustomerDto: CreateCustomerDto) {
     return 'This action adds a new customer';
   }
 
   findAll() {
-    return `This action returns all customers`;
+    return this.customerRepository.find();
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} customer`;
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
+  update(id: string, updateCustomerDto: UpdateCustomerDto) {
     return `This action updates a #${id} customer`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} customer`;
   }
 }
--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -1,43 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsPhoneNumber,
-  IsString,
-  Matches,
-} from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateCustomerDto } from './create-customer.dto';
 
-export class UpdateCustomerDto {
-  @IsString()
-  @IsOptional()
-  @IsNotEmpty()
-  @Matches(/^(?!\s*$).+/, { message: 'Name cannot be just spaces' })
-  @ApiPropertyOptional({
-    description: "The customer's name",
-    example: 'José',
-  })
-  name: string;
-
-  @IsOptional()
-  @IsEmail()
-  @IsNotEmpty()
-  @Matches(/^(?!\s*$).+/, { message: 'Email cannot be just spaces' })
-  @ApiPropertyOptional({
-    description: "The customer's email address",
-    example: 'customer@example.com',
-  })
-  email: string;
-
-  @IsOptional()
-  @IsPhoneNumber('BR', {
-    message: 'Phone number must be a valid phone number in Brazil',
-  })
-  @IsNotEmpty()
-  @Matches(/^(?!\s*$).+/, { message: 'Phone cannot be just spaces' })
-  @ApiPropertyOptional({
-    description: "The customer's phone number",
-    example: '+55 11 91234-5678',
-  })
-  phone: string;
-}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
