b5b255ca42be7e14081cec3d1cc66112c5578765
Sara Ferreira <sara.f.araujo12@gmail.com>
2025-02-18 16:01:47 -0300

    feat: created customers dtos

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,39 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+} from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Name cannot be just spaces' })
+  @ApiProperty({
+    description: "The customer's name",
+    example: 'José',
+  })
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Email cannot be just spaces' })
+  @ApiProperty({
+    description: "The customer's email address",
+    example: 'jose@example.com',
+  })
+  email: string;
+
+  @IsPhoneNumber('BR', {
+    message: 'Phone number must be a valid phone number in Brazil',
+  })
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Phone cannot be just spaces' })
+  @ApiProperty({
+    description: "The customer's phone number",
+    example: '+55 11 91234-5678',
+  })
+  phone: string;
+}
--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -1,4 +1,43 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCustomerDto } from './create-customer.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+} from 'class-validator';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
+export class UpdateCustomerDto {
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Name cannot be just spaces' })
+  @ApiPropertyOptional({
+    description: "The customer's name",
+    example: 'José',
+  })
+  name: string;
+
+  @IsOptional()
+  @IsEmail()
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Email cannot be just spaces' })
+  @ApiPropertyOptional({
+    description: "The customer's email address",
+    example: 'customer@example.com',
+  })
+  email: string;
+
+  @IsOptional()
+  @IsPhoneNumber('BR', {
+    message: 'Phone number must be a valid phone number in Brazil',
+  })
+  @IsNotEmpty()
+  @Matches(/^(?!\s*$).+/, { message: 'Phone cannot be just spaces' })
+  @ApiPropertyOptional({
+    description: "The customer's phone number",
+    example: '+55 11 91234-5678',
+  })
+  phone: string;
+}
